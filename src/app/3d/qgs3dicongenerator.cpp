4ed95cf562e8f3ecbee8e9d451a2b312262ee0f5
Nyall Dawson <nyall.dawson@gmail.com>
2020-07-29 10:31:58 +1000

    More wip

--- src/app/3d/qgs3dicongenerator.cpp
+++ src/app/3d/qgs3dicongenerator.cpp
@@ -25,6 +25,8 @@
 #include "qgs3dutils.h"
 #include "qgs3dmapscene.h"
 #include "qgscameracontroller.h"
+#include "qgsmultipolygon.h"
+#include "qgspolygon.h"
 
 Qgs3DIconGenerator::Qgs3DIconGenerator( QObject *parent )
   : QgsAbstractStyleEntityIconGenerator( parent )
@@ -37,15 +39,30 @@ void Qgs3DIconGenerator::generateIcon( QgsStyle *style, QgsStyle::StyleEntity ty
   if ( type != QgsStyle::Symbol3DEntity )
     return;
 
-  std::unique_ptr< QgsAbstract3DSymbol > symbol( style->symbol3D( name ) );
+  IconRequest request( style, type, name );
+  if ( mPendingRequests.contains( request ) )
+    return;
+
+  mPendingRequests.append( request );
+
+  if ( mGeneratingIcon )
+    return;
+
+  mGeneratingIcon = true;
+  generateIcon( mPendingRequests.first() );
+}
+
+void Qgs3DIconGenerator::generateIcon( const Qgs3DIconGenerator::IconRequest &request )
+{
+  std::unique_ptr< QgsAbstract3DSymbol > symbol( request.style->symbol3D( request.name ) );
 
   QgsRectangle fullExtent( 0, 0, 1000, 1000 );
 
-  std::unique_ptr< QgsVectorLayer > tempLayer;
+  QgsVectorLayer *tempLayer = nullptr;
 
-  if ( symbol->type() == QStringLiteral( "lines" ) )
+  if ( symbol->type() == QStringLiteral( "line" ) )
   {
-    tempLayer = qgis::make_unique< QgsVectorLayer>( "LineString?crs=EPSG:27700", "lines", "memory" );
+    tempLayer = new QgsVectorLayer( "LineString?crs=EPSG:27700", "lines", "memory" );
     QVector<QgsPoint> pts;
     pts << QgsPoint( 0, 0, 10 ) << QgsPoint( 0, 1000, 10 ) << QgsPoint( 1000, 1000, 10 ) << QgsPoint( 1000, 0, 10 );
     pts << QgsPoint( 1000, 0, 500 ) << QgsPoint( 1000, 1000, 500 ) << QgsPoint( 0, 1000, 500 ) << QgsPoint( 0, 0, 500 );
@@ -55,9 +72,51 @@ void Qgs3DIconGenerator::generateIcon( QgsStyle *style, QgsStyle::StyleEntity ty
     flist << f1;
     tempLayer->dataProvider()->addFeatures( flist );
   }
+  else if ( symbol->type() == QStringLiteral( "polygon" ) )
+  {
+    tempLayer = new QgsVectorLayer( "MultiPolygonZ?crs=EPSG:27700", "polygons", "memory" );
+
+    QgsMultiPolygon mp;
+    mp.addGeometry( new QgsPolygon( new QgsLineString( QVector< QgsPoint >() << QgsPoint( 0, 0, 0 )
+                                    << QgsPoint( 0, 1000, 0 )
+                                    << QgsPoint( 1000, 1000, 0 )
+                                    << QgsPoint( 1000, 0, 0 )
+                                    << QgsPoint( 0, 0, 0 ) ) ) );
+    mp.addGeometry( new QgsPolygon( new QgsLineString( QVector< QgsPoint >() << QgsPoint( 0, 0, 0 )
+                                    << QgsPoint( 0, 1000, 0 )
+                                    << QgsPoint( 0, 1000, 500 )
+                                    << QgsPoint( 0, 0, 500 )
+                                    << QgsPoint( 0, 0, 0 ) ) ) );
+    mp.addGeometry( new QgsPolygon( new QgsLineString( QVector< QgsPoint >() << QgsPoint( 0, 0, 0 )
+                                    << QgsPoint( 1000, 0, 0 )
+                                    << QgsPoint( 1000, 0, 500 )
+                                    << QgsPoint( 0, 0, 500 )
+                                    << QgsPoint( 0, 0, 0 ) ) ) );
+    mp.addGeometry( new QgsPolygon( new QgsLineString( QVector< QgsPoint >() << QgsPoint( 0, 1000, 0 )
+                                    << QgsPoint( 1000, 1000, 0 )
+                                    << QgsPoint( 1000, 1000, 500 )
+                                    << QgsPoint( 0, 1000, 500 )
+                                    << QgsPoint( 0, 1000, 0 ) ) ) );
+    mp.addGeometry( new QgsPolygon( new QgsLineString( QVector< QgsPoint >() << QgsPoint( 1000, 0, 0 )
+                                    << QgsPoint( 1000, 1000, 0 )
+                                    << QgsPoint( 1000, 1000, 500 )
+                                    << QgsPoint( 1000, 0, 500 )
+                                    << QgsPoint( 1000, 0, 0 ) ) ) );
+    mp.addGeometry( new QgsPolygon( new QgsLineString( QVector< QgsPoint >() << QgsPoint( 0, 0, 500 )
+                                    << QgsPoint( 0, 1000, 500 )
+                                    << QgsPoint( 1000, 1000, 500 )
+                                    << QgsPoint( 1000, 0, 500 )
+                                    << QgsPoint( 0, 0, 500 ) ) ) );
+
+    QgsFeature f1( tempLayer->fields() );
+    f1.setGeometry( QgsGeometry( mp.clone() ) );
+    QgsFeatureList flist;
+    flist << f1;
+    tempLayer->dataProvider()->addFeatures( flist );
+  }
   else if ( symbol->type() == QStringLiteral( "point" ) )
   {
-    tempLayer = qgis::make_unique< QgsVectorLayer>( "PointZ?crs=EPSG:27700", "lines", "memory" );
+    tempLayer = new QgsVectorLayer( "PointZ?crs=EPSG:27700", "lines", "memory" );
     QgsFeature f1( tempLayer->fields() );
     f1.setGeometry( QgsGeometry( new QgsPoint( 500, 500, 200 ) ) );
     QgsFeatureList flist;
@@ -65,39 +124,120 @@ void Qgs3DIconGenerator::generateIcon( QgsStyle *style, QgsStyle::StyleEntity ty
     tempLayer->dataProvider()->addFeatures( flist );
   }
   else
+  {
+    mPendingRequests.removeAll( request );
+    if ( mPendingRequests.empty() )
+      mGeneratingIcon = false;
+    else
+      generateIcon( mPendingRequests.first() );
     return;
+  }
 
   tempLayer->setRenderer3D( new QgsVectorLayer3DRenderer( symbol.release() ) );
-
 
   Qgs3DMapSettings *map = new Qgs3DMapSettings;
   map->setCrs( tempLayer->crs() );
   map->setOrigin( QgsVector3D( fullExtent.center().x(), fullExtent.center().y(), 0 ) );
-  map->setLayers( QList<QgsMapLayer *>() << tempLayer.get() );
+  map->setLayers( QList<QgsMapLayer *>() << tempLayer );
+  QgsPointLightSettings light;
+  light.setPosition( QVector3D( 1500, 1000, 1300 ) );
+  map->setPointLights( QList< QgsPointLightSettings >() << light );
 
   QgsFlatTerrainGenerator *flatTerrain = new QgsFlatTerrainGenerator;
   flatTerrain->setCrs( map->crs() );
   flatTerrain->setExtent( fullExtent );
   map->setTerrainGenerator( flatTerrain );
 
-  QgsOffscreen3DEngine engine;
-  Qgs3DMapScene *scene = new Qgs3DMapScene( *map, &engine );
-  engine.setRootEntity( scene );
-
+  QgsOffscreen3DEngine *engine = new QgsOffscreen3DEngine();
+  Qgs3DMapScene *scene = new Qgs3DMapScene( *map, engine );
+  engine->setRootEntity( scene );
   scene->cameraController()->setLookingAtPoint( QgsVector3D( 0, 0, 0 ), 2500, 45, 45 );
+  engine->setClearColor( QColor( 0, 100, 0 ) );
 
-  QImage img = Qgs3DUtils::captureSceneImage( engine, scene );
-
-  QIcon icon;
   const QList< QSize > sizes = iconSizes();
   if ( sizes.isEmpty() )
-    icon.addFile( QgsApplication::defaultThemePath() + QDir::separator() + QStringLiteral( "3d.svg" ), QSize( 24, 24 ) );
-  for ( const QSize &s : sizes )
+    engine->setSize( QSize( 24, 24 ) );
+  else
   {
-    QImage scaled = img.scaled( s.width(), s.height() );
-    icon.addPixmap( QPixmap::fromImage( scaled ) );
-//    icon.addFile( QgsApplication::defaultThemePath() + QDir::separator() + QStringLiteral( "3d.svg" ), s );
+    QSize largest( 0, 0 );
+    for ( const QSize size : sizes )
+    {
+      if ( size.width() > largest.width() )
+        largest.setWidth( size.width() );
+      if ( size.height() > largest.height() )
+        largest.setHeight( size.height() );
+    }
+    engine->setSize( QSize( largest.width() * 3, largest.height() * 3 ) );
   }
 
-  emit iconGenerated( type, name, icon );
+  auto requestImageFcn = [engine, scene]
+  {
+    if ( scene->sceneState() == Qgs3DMapScene::Ready )
+    {
+      engine->requestCaptureImage();
+    }
+  };
+
+  auto saveImageFcn = [this, scene, engine, map, request, tempLayer]( const QImage & img )
+  {
+    scene->deleteLater();
+    engine->deleteLater();
+    tempLayer->deleteLater();
+    delete map;
+
+    QIcon icon;
+    const QList< QSize > sizes = iconSizes();
+    if ( sizes.isEmpty() )
+    {
+      QImage scaled = img.scaled( 24, 24 );
+      icon.addPixmap( QPixmap::fromImage( scaled ) );
+    }
+    else
+    {
+      for ( const QSize &s : sizes )
+      {
+        QImage scaled( s, QImage::Format_ARGB32 );
+        if ( s.width() > 30 )
+        {
+          scaled.fill( Qt::transparent );
+          QPainter p( &scaled );
+          p.setBrush( QBrush( Qt::black ) );
+          p.setPen( Qt::NoPen );
+          p.setRenderHint( QPainter::Antialiasing, true );
+          p.drawRoundedRect( scaled.rect(), 6, 6 );
+          p.end();
+        }
+        else
+        {
+          scaled.fill( Qt::black );
+        }
+
+        QPainter p( &scaled );
+        p.setCompositionMode( QPainter::CompositionMode_SourceIn );
+        p.drawImage( scaled.rect(), img, img.rect() );
+        p.end();
+
+        icon.addPixmap( QPixmap::fromImage( scaled ) );
+      }
+    }
+
+    mPendingRequests.removeAll( request );
+    emit iconGenerated( request.type, request.name, icon );
+
+    if ( mPendingRequests.empty() )
+      mGeneratingIcon = false;
+    else
+      generateIcon( mPendingRequests.first() );
+  };
+
+  QObject::connect( engine, &QgsAbstract3DEngine::imageCaptured, saveImageFcn );
+  if ( scene->sceneState() == Qgs3DMapScene::Ready )
+  {
+    requestImageFcn();
+  }
+  else
+  {
+    // first wait until scene is loaded
+    QObject::connect( scene, &Qgs3DMapScene::sceneStateChanged, requestImageFcn );
+  }
 }
